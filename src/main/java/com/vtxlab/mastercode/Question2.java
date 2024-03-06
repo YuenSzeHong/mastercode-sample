6e6060ce62eb2b5c1f075d32e28024a20fd4385f
Yuen Sze Hong <szehong26@gmail.com>
2024-03-06 14:54:52 +0800

    207909

--- src/main/java/com/vtxlab/mastercode/Question2.java
+++ src/main/java/com/vtxlab/mastercode/Question2.java
@@ -3,7 +3,22 @@ package com.vtxlab.mastercode;
 public class Question2 {
     
     public int calculateTime(String keyboard, String word) {
-        return -1;
+
+        if (word.isEmpty()) {
+            return 0;
+        }
+
+        int time = 0;
+        int current = 0;
+
+        char[] wordChars = word.toCharArray();
+
+        for (char c : wordChars) {
+            int next = keyboard.indexOf(c);
+            time += Math.abs(next - current);
+            current = next;
+        }
+        return time;
     }
 
 }
--- src/main/java/com/vtxlab/mastercode/Question3.java
+++ src/main/java/com/vtxlab/mastercode/Question3.java
@@ -1,9 +1,24 @@
 package com.vtxlab.mastercode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Question3 {
 
     public int[] anagramMappings(int[] nums1, int[] nums2) {
-        return new int[] {};
+        Map<Integer,Integer> indesxMap = new HashMap<>();
+
+        for (int i = 0; i < nums2.length; i++) {
+            indesxMap.put(nums2[i], i);
+        }
+
+        int[] result = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = indesxMap.get(nums1[i]);
+        }
+
+        return result;
     }
     
 }
